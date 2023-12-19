f6bafd1dc832a2b473b50c4b501e1dc3d7a2c5bf
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2023-12-19 14:12:23 +0100

    Initial commit

---
+++ Java_Intro3/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double a = 1.3;
+        double b = 2.4;
+        int somma = (int) (a + b);
+        System.out.println(somma);
+
+    }
+}
